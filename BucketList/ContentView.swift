618a12afee7df129f3d798a38b9cbd0833753dc3
Hariom <hariomconnects03@gmail.com>
2025-06-26 09:31:10 +0530

    Feature : Implemented the comparable and sorted() for the whole app to use added the comparing funciton in User to compare the properties of the User

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
-struct User:Identifiable{
+struct User:Comparable,Identifiable{
     let id = UUID()
     var firstname: String
     var lastname: String
+    
+    // On this < - operator overloading i got asked question in interview
+    // When we use some operator for general use but here we are comparing the objects of user
+    // By conforming to comparable we got access to < & > in the func to use
+    
+    static func <(lhs:User, rhs:User)-> Bool{
+        lhs.lastname < rhs.lastname
+    }
+    // Now swift knows that when i call sorted() on users then it should use this function to compare 
 }
 
 struct ContentView: View {
@@ -17,9 +26,8 @@ struct ContentView: View {
         User(firstname: "David", lastname: "Schwarznegar"),
         User(firstname: "Arnold", lastname: "Burger"),
         User(firstname: "Ketty", lastname: "Perry")
-    ].sorted{
-        $1.firstname < $0.firstname
-    }
+    ].sorted()
+    
     var body: some View {
         List(users){ user in
             Text("\(user.lastname), \(user.firstname)")
