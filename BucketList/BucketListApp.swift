d516e6a5cb3b308c76572449be119403d619cd26
Hariom <hariomconnects03@gmail.com>
2025-06-26 08:51:18 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Hari's Mac on 26.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
